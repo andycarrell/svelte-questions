6f7d7b81bac71b8001b5f3c285ed4c2d16bca13e
Andy Carrell <andy@jasper.io>
2019-10-06 12:31:08 +1300

    Extracts questions as its own component, renders navigation with the ability to switch between

---
+++ src/Navigation.svelte
@@ -0,0 +1,57 @@
+<script>
+	import { navigation } from "./navigationStore.js"
+
+	const items = [
+		"questions",
+		"sandbox",
+	];
+</script>
+
+<style>
+	nav {
+		width: 100%;
+		text-align: center;
+		margin: 8px auto;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0px 12px;
+	}
+
+	li {
+		display: inline;
+	}
+
+	button {
+		margin: 0px 4px;
+		border: none;
+		background-color: transparent;
+		cursor: pointer;
+		color: gray;
+	}
+
+	button.active {
+		color: slategray;
+	}
+
+	button:hover,:focus {
+		color: black;
+		text-decoration: underline;
+	}
+</style>
+
+<nav>
+	<ul>
+		{#each items as item}
+			<li>
+				<button
+					class="{$navigation === item && "active"}"
+					on:click="{() => { $navigation = item }}"
+				>
+					{item}
+				</button>
+			</li>
+		{/each}
+	</ul>
+</nav>
